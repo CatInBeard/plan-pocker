70faac0e3f3a45724052ce4d60a622dc6eb49b4f
Grigoriy Efimov <efimov-gm@newpage.xyz>
2025-06-16 23:02:02 +0300

    fix deploy images

--- build-cloud-docker.sh
+++ build-cloud-docker.sh
@@ -3,3 +3,4 @@ docker build -t plan-pocker-websocket-server -f ./cloud/websocket-server/Dockerf
 docker build -t plan-pocker-frontend         -f ./cloud/frontend/Dockerfile         .
 docker build -t plan-pocker-game             -f ./cloud/game/Dockerfile             .
 docker build -t plan-pocker-api-server       -f ./cloud/api-server/Dockerfile       .
+docker build -t plan-pocker-nginx            -f ./cloud/nginx/Dockerfile            ./cloud/nginx/
